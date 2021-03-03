f6a03997ffed41baa3146f9a9dd979b448e6fbf2
amansohal8654 <amansohal8654@gmail.com>
2021-03-03 14:54:04 +1300

    fix an runtime error

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ int main() {
     int roadOne, roadTwo, roadThree, roadFour; 
     int roundabout, stopsigns, trafficlights;
     
-    cout << "Cars-Per-Minute (CPM) rate for Road 1 "; // Type a number and press enter
+    cout << "Cars-Per-Minute (CPM) rate for Road 1 \n"; // Type a number and press enter
     cin >> roadOne; // Get user input from the keyboard
-    cout << "Cars-Per-Minute (CPM) rate for Road 2 "; // Display the input value
+    cout << "Cars-Per-Minute (CPM) rate for Road 2 \n"; // Display the input value
     cin >> roadTwo;
-    cout << "Cars-Per-Minute (CPM) rate for Road 3 "; // Type a number and press enter
+    cout << "Cars-Per-Minute (CPM) rate for Road 3 \n"; // Type a number and press enter
     cin >> roadThree; 
-    cout << "Cars-Per-Minute (CPM) rate for Road 4 "; // Type a number and press enter
+    cout << "Cars-Per-Minute (CPM) rate for Road 4 \n"; // Type a number and press enter
     cin >> roadFour; 
 
     int TotalCpm = roadOne + roadTwo + roadThree + roadFour;
@@ -21,7 +21,7 @@ int main() {
         stopsigns = 20;
         trafficlights = 90;
 
-    } else if(20 > TotalCpm || TotalCpm >= 10){
+    } else if(20 > TotalCpm && TotalCpm >= 10){
         roundabout = 75;
         stopsigns = 30;
         trafficlights = 75;
