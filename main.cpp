0479ce4063fca69a6fdc88754cc07cfed7f73058
amansohal8654 <amansohal8654@gmail.com>
2021-03-03 14:28:16 +1300

    c++ task 1

---
+++ main.cpp
@@ -0,0 +1,44 @@
+ #include <iostream>
+using namespace std;
+
+int main() {
+    int roadOne, roadTwo, roadThree, roadFour; 
+    int roundabout, stopsigns, trafficlights;
+    cout << "Cars-Per-Minute (CPM) rate for Road 1 "; // Type a number and press enter
+    cin >> roadOne; // Get user input from the keyboard
+    cout << "Cars-Per-Minute (CPM) rate for Road 2 " << x; // Display the input value
+    cin >> roadTwo;
+    cout << "Cars-Per-Minute (CPM) rate for Road 3 "; // Type a number and press enter
+    cin >> roadThree; 
+    cout << "Cars-Per-Minute (CPM) rate for Road 4 "; // Type a number and press enter
+    cin >> roadFour; 
+
+    int TotalCpm = roadOne + roadTwo + roadThree + roadFour;
+
+    if(TotalCpm >= 20 ){
+        roundabout = 50
+        stopsigns = 20
+        trafficlights = 90
+
+    } else if(20 > TotalCpm || TotalCpm >= 10){
+        roundabout = 75
+        stopsigns = 30
+        trafficlights = 75
+
+    } else if(TotalCpm < 10){
+        roundabout = 90
+        stopsigns = 40
+        trafficlights = 30
+    }
+
+    if(roundabout > stopsigns && roundabout > trafficlights){
+        cout << "roundabout"
+    } else if( roundabout == trafficlights){
+        cout << "roundabout or trafficlights"
+    } else if(stopsigns > trafficlights){
+        cout << "stopsigns"
+    } else{
+        cout << "trafficlights"
+    }
+    return 0;
+} 
